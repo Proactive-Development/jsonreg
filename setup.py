f244cb31b88d5a1bbc7fc30dcaad0ac2a66bce74
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2022-03-02 21:10:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author='Lewis Evans',
     packages=['jsonreg'],
     install_requires=[''],
-    version="0.1.4",
+    version="0.1.5",
     license='GNU',
     long_description=long_description,
     long_description_content_type="text/markdown",
